82e20242b21f244aafde7080a64b821288a69345
ajayyadav201 <91079604+ajayyadav201@users.noreply.github.com>
2021-09-20 23:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ from Adafruit_IO import Client
 aio = Client(Username,Active_Key)
 
  
-def vedh1(bot,update):
+def Ajayyadavbot1(bot,update):
   chat_id = bot.message.chat_id
   bot.message.reply_text('light turned on')
   aio.send('ajaymajor2',1)
  
-def vedh2(bot,update):
+def Ajayyadavbot2(bot,update):
   chat_id = bot.message.chat_id
   bot.message.reply_text('light turned off')
   aio.send('ajaymajor2',0)
  
-def vedh3(bot,update):
+def Ajayyadavbot3(bot,update):
   chat_id = bot.message.chat_id
   bot.message.reply_text('Fan turned on')
   aio.send('ajaymajor2',2)
  
-def vedh4(bot,update):
+def Ajayyadavbot4(bot,update):
   chat_id = bot.message.chat_id
   bot.message.reply_text('Fan turned off')
   aio.send('ajaymajor2',3)
@@ -34,13 +34,13 @@ def main(bot,update):
   a= bot.message.text.lower()
   
   if a =="turn on light":
-    vedh1(bot,update)
+    Ajayyadavbot1(bot,update)
   elif a =="turn off light":
-    vedh2(bot,update)
+    Ajayyadavbot2(bot,update)
   elif a =="turn on fan":
-    vedh3(bot,update)
+    Ajayyadavbot3(bot,update)
   elif a =="turn off fan":
-    vedh4(bot,update) 
+    Ajayyadavbot4(bot,update) 
      
 
 u = Updater(bot_token,use_context=True)
